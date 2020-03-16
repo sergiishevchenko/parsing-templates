808e93a23c1cb74a3868e8fa6e61d1847c1d0339
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-03-16 20:53:39 +0100

    third template

---
+++ main_v3.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def write_csv(data):
+    with open('cmc.csv', 'a') as f:
+        writer = csv.writer(f)
+
+        writer.writerow([data['name'],
+                        data['symbol'],
+                        data['url'],
+                        data['price']])
+
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    trs = soup.find_all('table')[2].find('tbody').find_all('tr')
+    for tr in trs:
+        tds = tr.find_all('td')
+        name = tds[1].find('a', class_='cmc-link').text
+        symbol = tds[1].find_all('img')[0].get('src')
+        url = 'https://coinmarketcap.com' + tds[1].find('a').get('href')
+        price = tds[3].find('a').text
+
+        data = {'name': name,
+                'symbol': symbol,
+                'url': url,
+                'price': price}
+
+        write_csv(data)
+
+
+def main():
+    url = 'https://coinmarketcap.com/'
+    get_page_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
