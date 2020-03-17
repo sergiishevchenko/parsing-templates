c57bb6a276cfac93310dc7fc8540e20b6890d0e1
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-03-17 09:39:54 +0100

    sixth template (DictWriter)

---
+++ main_v6.py
@@ -0,0 +1,28 @@
+import csv
+
+def write_csv(data):
+    with open('names.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow((data['name'], data['surname'], data['age']))
+
+
+def write_csv2(data):
+    with open('names.csv', 'a') as file:
+        order = ['name', 'surname', 'age']
+        writer = csv.DictWriter(file, fieldnames=order)
+
+        writer.writerow(data)
+
+def main():
+    d = {'name': 'Peter', 'surname': 'Ivanov', 'age': 21}
+    d1 = {'name': 'Ivan', 'surname': 'Ivanov', 'age': 18}
+    d2 = {'name': 'Ksu', 'surname': 'Petrova', 'age': 32}
+
+    l = [d, d1, d2]
+
+    for i in l:
+        write_csv2(i)
+
+
+if __name__ == '__main__':
+    main()
